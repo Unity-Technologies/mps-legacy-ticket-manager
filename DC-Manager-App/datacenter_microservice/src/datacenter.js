b7bc90cb393e0bb01cc07d9d7c10dfea7bb56e0c
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-04-25 16:08:34 +0100

    Create database interface and database adapter classes for each data center

--- DC-Manager-App/datacenter_microservice/src/datacenter.js
+++ DC-Manager-App/datacenter_microservice/src/datacenter.js
@@ -1,91 +1,114 @@
-require("dotenv").config({path:"../../.env"})
-const axios = require('axios');
-const fs = require('fs');
+require("dotenv").config({ path: "../../.env" });
+const axios = require("axios");
+const fs = require("fs");
+const _100TBDataCenterAdapter = require("./datacenter_adapters/hundredtb-datacenter-adapter.js");
 
 const apiKey = process.env.HUNDREDTB_API_KEY;
 
 const allowedFileTypes = [
-    "image/jpg",
-    "image/jpeg",
-    "image/png",
-    "image/tiff",
-    "image/gif",
-    "text/csv",
-    "text/plain",
-    "video/3gpp",
-    "video/3gpp2",
-    "video/h261",
-    "video/h263",
-    "video/h264",
-    "video/jpeg",
-    "video/mp4",
-    "video/mpeg",
-    "video/ogg",
-    "video/quicktime",
-    "video/webm",
-    "application/pdf",
-    "application/x-pdf",
-    "application/csv",
-    "application/vnd.ms-excel",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/tiff",
+  "image/gif",
+  "text/csv",
+  "text/plain",
+  "video/3gpp",
+  "video/3gpp2",
+  "video/h261",
+  "video/h263",
+  "video/h264",
+  "video/jpeg",
+  "video/mp4",
+  "video/mpeg",
+  "video/ogg",
+  "video/quicktime",
+  "video/webm",
+  "application/pdf",
+  "application/x-pdf",
+  "application/csv",
+  "application/vnd.ms-excel",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 ];
 
 function isValidFileType(attachment) {
-    return allowedFileTypes.includes(attachment.mime);
+  return allowedFileTypes.includes(attachment.mime);
 }
-  
-async function createTicket(subject, body, department = 10, priority = 1, attachments = []) {
-    
-    if (attachments.length > 2) {
-        throw new Error('Maximum 2 attachments allowed per ticket.');
-    }
 
-    const requestBody = {
-        subject: subject,
-        body: body,
-        department: department,
-        priority: priority,
-        attachments: attachments.map(attachment => {
-            if (!isValidFileType(attachment)) {
-                throw new Error(`Invalid attachment file type: ${attachment.mime}. Allowed types are: ${allowedFileTypes.join(', ')}`);
-            }
-            return {
-                mime: attachment.mime,
-                name: attachment.name,
-                file: fs.readFileSync(attachment.path, 'base64'),
-            };
-        }),
-    };
+async function createTicket(
+  subject,
+  body,
+  department = 10,
+  priority = 1,
+  attachments = []
+) {
+  if (attachments.length > 2) {
+    throw new Error("Maximum 2 attachments allowed per ticket.");
+  }
 
-    console.log(requestBody);
+  const requestBody = {
+    subject: subject,
+    body: body,
+    department: department,
+    priority: priority,
+    attachments: attachments.map((attachment) => {
+      if (!isValidFileType(attachment)) {
+        throw new Error(
+          `Invalid attachment file type: ${
+            attachment.mime
+          }. Allowed types are: ${allowedFileTypes.join(", ")}`
+        );
+      }
+      return {
+        mime: attachment.mime,
+        name: attachment.name,
+        file: fs.readFileSync(attachment.path, "base64"),
+      };
+    }),
+  };
 
-    // Make the Axios POST request to create a new ticket
-    const response = await axios.post('https://api.ingenuitycloudservices.com/rest-api/tickets', 
-    requestBody, 
+  console.log(requestBody);
+
+  // Make the Axios POST request to create a new ticket
+  const response = await axios.post(
+    "https://api.ingenuitycloudservices.com/rest-api/tickets",
+    requestBody,
     {
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'X-Api-Token': `${apiKey}`
-        },
-    });
-
-    console.log(response.status);
-    console.log(response.data);
-
-    if (response.status === 200 || response.status === 201) {
-        // Ticket creation successful
-        return response.data; // Return the response data (might contain ticket details)
-    }   else if (response.status === 401) {
-        console.error('401 Unauthenticated Error, Response Message:', response.data.message)
-    }   else if (response.status === 413) {
-        console.error('413 Request Entity Too Large, Response Message:', response.data.message)
-        throw new Error('Attachment size exceeds the allowed limit. Please reduce the file size and try again.');
-    }   else {
-        // Handle error - log the error message and potentially throw an exception
-        console.error(`${response.status} Error creating ticket:`, response.data.message);
-        throw new Error('Failed to create ticket');
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-Api-Token": `${apiKey}`,
+      },
     }
+  );
+
+  console.log(response.status);
+  console.log(response.data);
+
+  if (response.status === 200 || response.status === 201) {
+    // Ticket creation successful
+    return response.data; // Return the response data (might contain ticket details)
+  } else if (response.status === 401) {
+    console.error(
+      "401 Unauthenticated Error, Response Message:",
+      response.data.message
+    );
+  } else if (response.status === 413) {
+    console.error(
+      "413 Request Entity Too Large, Response Message:",
+      response.data.message
+    );
+    throw new Error(
+      "Attachment size exceeds the allowed limit. Please reduce the file size and try again."
+    );
+  } else {
+    // Handle error - log the error message and potentially throw an exception
+    console.error(
+      `${response.status} Error creating ticket:`,
+      response.data.message
+    );
+    throw new Error("Failed to create ticket");
+  }
 }
 
 // Test Case 1: Valid Ticket Creation
@@ -101,20 +124,58 @@ async function createTicket(subject, body, department = 10, priority = 1, attach
 // console.log(response3);  // This should be similar to your mockApiResponse (assuming function handles long text)
 
 const mockAttachment1 = {
-    mime: 'image/png',
-    name: 'test_image.png',
-    path: 'C:\\Users\\juliantitusglover\\repos\\DC-Ticket-Manager-App\\DC-Manager-App\\assets\\logo.png',
+  mime: "image/png",
+  name: "test_image.png",
+  path: "C:\\Users\\juliantitusglover\\repos\\DC-Ticket-Manager-App\\DC-Manager-App\\assets\\logo.png",
 }; // Your mock attachment object
 
-const mockAttachment2 = {
-    mime: 'application/pdf',
-    name: 'test_file.pdf',
-    path: 'C:\\Users\\juliantitusglover\\Downloads\\Mastering GUI Programming with Python_ Develop impressive -- Alan D. Moore -- 2019 -- Packt Publishing -- 9781789612905 -- da31eb8d5c1afcdaafebffdd245898b9 -- Anna’s Archive.pdf',
+// const mockAttachment2 = {
+//   mime: "application/pdf",
+//   name: "test_file.pdf",
+//   path: "C:\\Users\\juliantitusglover\\Downloads\\Mastering GUI Programming with Python_ Develop impressive -- Alan D. Moore -- 2019 -- Packt Publishing -- 9781789612905 -- da31eb8d5c1afcdaafebffdd245898b9 -- Anna’s Archive.pdf",
+// };
+
+// // const response = createTicket('Test Ticket', 'Test ticket with Attachment', 10, 1, [mockAttachment1]);
+// // console.log(response);  // Should be similar to mockApiResponse
+
+// const response = createTicket(
+//   "Test Ticket with Large Attachment",
+//   "Body with Attachment",
+//   undefined,
+//   undefined,
+//   [mockAttachment2]
+// );
+
+// console.log(response); // Observe the output for successful or error messages
+
+const config = {
+  baseUrl: "https://api.ingenuitycloudservices.com/rest-api", // Replace with your actual API endpoint
+  apiKey: `${process.env.HUNDREDTB_API_KEY}`, // Replace with your actual API key
 };
 
-// const response = createTicket('Test Ticket', 'Test ticket with Attachment', 10, 1, [mockAttachment1]);
-// console.log(response);  // Should be similar to mockApiResponse
+const hundredtbAdapter = new _100TBDataCenterAdapter(config);
 
-const response = createTicket('Test Ticket with Large Attachment', 'Body with Attachment', undefined, undefined, [mockAttachment2]);
+const ticketData = {
+  subject: "Test Ticket with Attachment",
+  body: "Body with Attachment",
+  department: undefined,
+  priority: undefined, 
+  attachments: [mockAttachment1],
+};
 
-console.log(response);  // Observe the output for successful or error messages
+try {
+  hundredtbAdapter.createTicket(ticketData);
+//   console.log("Ticket created successfully:", createdTicket);
+} catch (error) {
+  console.error("Error creating ticket:", error.message);
+}
+
+
+const ticketId = 36663365; // Replace with the actual ticket ID
+
+try {
+  hundredtbAdapter.getTicket(ticketId);
+//   console.log("Fetched ticket details:", retrievedTicket);
+} catch (error) {
+  console.error("Error retrieving ticket:", error.message);
+}
---
+++ DC-Manager-App/datacenter_microservice/src/datacenter_adapters/hundredtb-datacenter-adapter.js
@@ -0,0 +1,148 @@
+const DataCenterClient = require("../interfaces/datacenter-interface.js");
+const axios = require("axios"); // Assuming you have axios installed for making HTTP requests
+const fs = require("fs"); // Assuming you need fs for handling file attachments
+
+const allowedFileTypes = [
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/tiff",
+  "image/gif",
+  "text/csv",
+  "text/plain",
+  "video/3gpp",
+  "video/3gpp2",
+  "video/h261",
+  "video/h263",
+  "video/h264",
+  "video/jpeg",
+  "video/mp4",
+  "video/mpeg",
+  "video/ogg",
+  "video/quicktime",
+  "video/webm",
+  "application/pdf",
+  "application/x-pdf",
+  "application/csv",
+  "application/vnd.ms-excel",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+];
+
+function isValidFileType(attachment) {
+  return allowedFileTypes.includes(attachment.mime);
+}
+
+class _100TBDataCenterAdapter extends DataCenterClient {
+  constructor(config) {
+    super(config.baseUrl, config.apiKey); // Call base class constructor
+  }
+
+  async createTicket(data) {
+    // Extract relevant data from the input argument
+    const {
+      subject,
+      body,
+      department = 10,
+      priority = 1,
+      attachments = [],
+    } = data;
+
+    if (attachments.length > 2) {
+      throw new Error("Maximum 2 attachments allowed per ticket.");
+    }
+
+    try {
+      const requestBody = {
+        // Map data to the format required by the 100TB API
+        subject,
+        body,
+        department,
+        priority,
+        attachments: attachments.map((attachment) => {
+          if (!isValidFileType(attachment)) {
+            throw new Error(
+              `Invalid attachment file type: ${
+                attachment.mime
+              }. Allowed types are: ${allowedFileTypes.join(", ")}`
+            );
+          }
+          return {
+            // Adapt attachment data to the format expected by the 100TB API
+            mime: attachment.mime,
+            name: attachment.name,
+            file: fs.readFileSync(attachment.path, "base64"),
+          };
+        }),
+      };
+
+      const response = await axios.post(
+        `${this.baseUrl}/tickets`,
+        requestBody,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "X-Api-Token": `${this.apiKey}`,
+          },
+        }
+      );
+
+      // Handle the response based on the 100TB API's specific codes and structure
+      if (response.status === 200 || response.status === 201) {
+        console.log(response.data);
+        return response.data;
+      } else if (response.status === 401) {
+        console.error(
+          "401 Unauthenticated Error, Response Message:",
+          response.data.message
+        );
+      } else {
+        // Handle errors based on the response status code and message
+        console.error(
+          `${response.status} Error creating ticket:`,
+          response.data.message
+        );
+        throw new Error(`${response.status} Failed to create ticket: ${response.data.message}`);
+      }
+    } catch (error) {
+      // Handle errors appropriately (e.g., log the error and re-throw)
+      console.error("Error creating ticket:", error.message);
+      throw error;
+    }
+  }
+
+  async getTicket(ticketId) {
+    try {
+      const response = await axios.get(`${this.baseUrl}/tickets/${ticketId}`, {
+        headers: {
+          Accept: "application/json",
+          "X-Api-Token": `${this.apiKey}`,
+        },
+      });
+
+      if (response.status === 200 || response.status === 201) {
+        console.log(`Fetched 100TB Ticket #${ticketId}:`, response.data);
+        // Ticket Retrieval successful
+        return response.data;
+      } else if (response.status === 401) {
+        console.error(
+          "401 Unauthenticated Error, Response Message:",
+          response.data.message
+        );
+      } else {
+        // Handle errors based on the response status code and message
+        console.error(
+          `${response.status} Error retrieving ticket:`,
+          response.data.message
+        );
+        throw new Error(`${response.status} Failed to retrieve ticket: ${response.data.message}`);
+      }
+    } catch (error) {
+      // Handle errors appropriately (e.g., log the error and re-throw)
+      console.error("Error retrieving ticket:", error.message);
+      throw error;
+    }
+  }
+}
+
+module.exports = _100TBDataCenterAdapter;
