f833a63574938e9f56f8c627fc6bee02e400bc5b
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-05-22 16:04:53 +0100

    Adjust INAP Adapter class method error handling based on tests

--- DC-Manager-App/datacenter_microservice/src/datacenter_adapters/inap-datacenter-adapter.js
+++ DC-Manager-App/datacenter_microservice/src/datacenter_adapters/inap-datacenter-adapter.js
@@ -9,11 +9,7 @@ class _InapDataCenterAdapter extends DataCenterClient {
 
   async createTicket(data) {
     // Extract relevant data from the input argument
-    const {
-        priority,
-        shortDescription,
-        description,
-    } = data;
+    const { priority, shortDescription, description } = data;
 
     try {
       const requestBody = {
@@ -29,8 +25,8 @@ class _InapDataCenterAdapter extends DataCenterClient {
         {
           headers: {
             "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${this.apiKey}`,
+            Accept: "application/json",
+            Authorization: `Bearer ${this.apiKey}`,
           },
         }
       );
@@ -39,60 +35,134 @@ class _InapDataCenterAdapter extends DataCenterClient {
       if (response.status === 200 || response.status === 201) {
         console.log(response.data);
         return response.data;
-      } else if (response.status === 401) {
-        console.error(
-          "401 Unauthenticated Error, Response Message:",
-          response.data.message
-        );
-      } else {
-        // Handle errors based on the response status code and message
-        console.error(
-          `${response.status} Error creating INAP ticket:`,
-          response.data.message
-        );
-        throw new Error(
-          `${response.status} Failed to create INAP ticket: ${response.data.message}`
-        );
       }
     } catch (error) {
-      // Handle errors appropriately (e.g., log the error and re-throw)
-      console.error("Error creating INAP ticket:", error.message);
-      throw error;
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls outside the validated range
+        console.error(
+          `${error.response.status} Error Creating ticket:`,
+          error.response.data.message
+        );
+
+        if (error.response.status === 400) {
+          // Handle specific 400 errors based on the message
+          if (error.response.data.message === "shortDescription is required") {
+            throw new Error("Error: Missing Ticket Subject");
+          } else if (
+            error.response.data.message === "description is required"
+          ) {
+            throw new Error("Error: Missing Ticket Body");
+          } else {
+            // Handle other 400 errors with a generic message
+            throw new Error(
+              `Failed to create ticket: ${error.response.data.message}`
+            );
+          }
+        } else if (error.response.status === 401) {
+          console.error(
+            "401 Unauthenticated Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `401 Error Failed to create ticket: ${error.response.data.message}`
+          );
+        } else if (error.response.status === 500) {
+          console.error(
+            "500 Internal Server Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `500 Error Failed to create ticket: ${error.response.data.message}`
+          );
+        } else {
+          // Handle other unexpected errors
+          throw new Error(`Failed to create ticket: ${error.response}`);
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Error creating ticket: No response received");
+        throw new Error("Network Error: Could not create ticket");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error creating ticket:", error);
+        throw error;
+      }
     }
   }
 
   async getTicket(ticketId) {
     try {
-      const response = await axios.get(`${this.baseUrl}/support-cases/${ticketId}`, {
-        headers: {
-          "Accept": "application/json",
-          "Authorization": `Bearer ${this.apiKey}`,
-        },
-      });
+      const response = await axios.get(
+        `${this.baseUrl}/support-cases/${ticketId}`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${this.apiKey}`,
+          },
+        }
+      );
 
       if (response.status === 200 || response.status === 201) {
         console.log(`Fetched INAP Ticket #${ticketId}:`, response.data);
         // Ticket Retrieval successful
         return response.data;
-      } else if (response.status === 401) {
-        console.error(
-          "401 Unauthenticated Error, Response Message:",
-          response.data.message
-        );
-      } else {
-        // Handle errors based on the response status code and message
-        console.error(
-          `${response.status} Error retrieving INAP ticket:`,
-          response.data.message
-        );
-        throw new Error(
-          `${response.status} Failed to retrieve INAP ticket: ${response.data.message}`
-        );
       }
     } catch (error) {
-      // Handle errors appropriately (e.g., log the error and re-throw)
-      console.error("Error retrieving INAP ticket:", error.message);
-      throw error;
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls outside the validated range
+        console.error(
+          `${error.response.status} Error Creating ticket:`,
+          error.response.data.message
+        );
+
+        if (error.response.status === 401) {
+          // Handle specific 400 errors based on the message
+          console.error(
+            "401 Unauthenticated Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `401 Error Failed to create ticket: ${error.response.data.message}`
+          );
+        } else if (error.response.status === 403) {
+          console.error(
+            "403 Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `403 Error Failed to retrieve ticket: ${error.response.data.message}`
+          );
+        } else if (error.response.status === 404) {
+          console.error(
+            "404 Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `404 Error Failed to retrieve ticket: ${error.response.data.message}`
+          );
+        } else if (error.response.status === 500) {
+          console.error(
+            "500 Internal Server Error, Response Message:",
+            error.response.data.message
+          );
+          throw new Error(
+            `500 Error Failed to create ticket: ${error.response.data.message}`
+          );
+        } else {
+          // Handle other unexpected errors
+          throw new Error(`Failed to create ticket: ${error.response}`);
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Error creating ticket: No response received");
+        throw new Error("Network Error: Could not create ticket");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error creating ticket:", error);
+        throw error;
+      }
     }
   }
 }
