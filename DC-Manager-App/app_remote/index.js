d2a2c192aebced241765b23cdfd4ddc14eb7a088
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-07-10 12:17:35 +0100

    Create entry point for application

--- DC-Manager-App/app_remote/index.js
+++ DC-Manager-App/app_remote/index.js
@@ -1,16 +1,28 @@
-const express = require('express');
+require('dotenv').config();
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const ticketRoutes = require('./routes/ticketRoutes');
+const path = require("path");
 
 const app = express();
 
-// Define a port number for the server
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
-// Basic route handler to display a welcome message
-app.get('/sidebar', (req, res) => {
-  res.send('Hello from your Zendesk application!');
+app.get("/sidebar", (req, res) => {
+  const qs = new URLSearchParams(req.query).toString();
+  res.cookie("my_app_params", qs, { httpOnly: true });
+  res.render("index", { qs });
 });
 
-// Start the server
+app.use('/tickets', ticketRoutes);
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
