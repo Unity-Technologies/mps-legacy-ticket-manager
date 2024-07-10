2c626e0ebfa04ae40949825a9abbecc19921ad4d
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-07-10 12:32:12 +0100

    Add resize functionality when DOM loads for dashboard page

---
+++ DC-Manager-App/app_remote/public/js/dashboard.js
@@ -0,0 +1,8 @@
+let client; // Declare the client variable globally
+
+document.addEventListener("DOMContentLoaded", function() {
+  client = ZAFClient.init();
+
+  client.invoke('resize', { width: '100%', height: '160px' });
+
+});
