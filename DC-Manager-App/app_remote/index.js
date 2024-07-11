c91ef537311f1017a8f0a049527c0da24088ea8b
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-07-11 11:51:42 +0100

    Adjust entry point of application to use dashboard routes and error handler middleware

--- DC-Manager-App/app_remote/index.js
+++ DC-Manager-App/app_remote/index.js
@@ -3,25 +3,28 @@ require('dotenv').config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const ticketRoutes = require('./routes/ticketRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
+const errorHandler = require('./middleware/errorHandler');
 const path = require("path");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 app.use("/static", express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-app.get("/sidebar", (req, res) => {
-  const qs = new URLSearchParams(req.query).toString();
-  res.cookie("my_app_params", qs, { httpOnly: true });
-  res.render("index", { qs });
-});
+app.use('/sidebar', dashboardRoutes);
 
 app.use('/tickets', ticketRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
