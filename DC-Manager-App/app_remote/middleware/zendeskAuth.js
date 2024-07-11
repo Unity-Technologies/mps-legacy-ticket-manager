46baf7f369eb197ed1acebae1d0bced2f108ad9d
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-07-11 10:55:59 +0100

    Create middleware to authenticate user before ticket submission

---
+++ DC-Manager-App/app_remote/middleware/zendeskAuth.js
@@ -0,0 +1,31 @@
+const zendeskService = require("../services/zendeskService");
+
+const checkZendeskAuth = async (req, res, next) => {
+    console.log(req.body);
+    const authEmail = req.body.zendeskUserEmail;
+    const userId = req.body.assigneeId;
+    let error_msg = "";
+
+    const qs = new URLSearchParams(req.query).toString();
+    res.cookie("my_app_params", qs, { httpOnly: true });
+  
+    if (!authEmail || !userId) {
+        error_msg = `Authentication details missing`;
+        return res.render("create-support-ticket", { qs, error_msg });
+    }
+  
+    try {
+      await zendeskService.verifyAuth(authEmail, userId).then(() => {
+        next();
+      }).catch((err) => {
+        error_msg = `Unauthorized: Invalid Zendesk credentials, ${err}`;
+        return res.render("create-support-ticket", { qs, error_msg });
+      });
+    } catch (error) {
+      console.error("Error verifying Zendesk auth:", error);
+      error_msg = `Internal Server Error (Zendesk Verification): ${error}`;
+      res.render("create-support-ticket", { qs, error_msg });
+    }
+  };
+  
+module.exports = checkZendeskAuth;
