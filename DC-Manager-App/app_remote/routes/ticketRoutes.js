aa66f883649d8d56cbce88ef915bf4a00d8cdc05
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-07-11 11:47:24 +0100

    Add upload attachment and Zendesk authentication middleware before ticket submission

--- DC-Manager-App/app_remote/routes/ticketRoutes.js
+++ DC-Manager-App/app_remote/routes/ticketRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const ticketController = require("../controllers/ticketController");
-const multer = require("multer");
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const uploadAttachments = require("../middleware/uploadAttachments");
+const checkZendeskAuth = require("../middleware/zendeskAuth");
 
 router.get("/new", ticketController.createTicket);
-router.post("/new", upload.array('attachments', 2), ticketController.submitTicket);
+router.post("/new", uploadAttachments, checkZendeskAuth, ticketController.submitTicket);
 router.get("/partials/:dataCenter", (req, res) => {
   const dataCenter = req.params.dataCenter;
   res.render(`partials/${dataCenter}-form`);
