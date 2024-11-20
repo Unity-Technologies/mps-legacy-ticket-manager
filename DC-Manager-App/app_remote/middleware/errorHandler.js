24d05e374aa6524006451b18bcc4516390ae0ff3
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-11-20 10:06:37 +0000

    Make error handler middleware more modular.

--- DC-Manager-App/app_remote/middleware/errorHandler.js
+++ DC-Manager-App/app_remote/middleware/errorHandler.js
@@ -1,8 +1,21 @@
-const errorHandler = (err, req, res) => {
-    console.error(err.stack);
-    const qs = new URLSearchParams(req.query).toString();
-    res.cookie("my_app_params", qs, { httpOnly: true });
-    res.status(500).render("internal-error-500", { qs });
-  };
-  
-  module.exports = errorHandler;
+/* eslint-disable no-unused-vars */
+const errorHandler = (err, req, res, next) => {
+  const status = err.status || 500; // Default to 500 if no status is provided
+  const message = err.message || "Internal Server Error";
+  const description = err.description || "";
+
+  console.error(err.stack);
+
+  const qs = new URLSearchParams(req.query).toString();
+  res.cookie("my_app_params", qs, { httpOnly: true });
+
+  // Render the modular error page
+  res.status(status).render("errors/error", {
+    qs,
+    status,
+    error_msg: message,
+    description
+  });
+};
+
+module.exports = errorHandler;
