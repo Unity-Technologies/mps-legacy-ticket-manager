de4ae0ae2d59308520980c1dc84e195fba7deadb
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-11-20 13:10:07 +0000

    Add check Session auth middleware and invoke Cloud Run Service

--- DC-Manager-App/app_remote/routes/dashboardRoutes.js
+++ DC-Manager-App/app_remote/routes/dashboardRoutes.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const checkZendeskAuth = require("../middleware/zendeskAuth");
+const checkSessionAuth = require("../middleware/checkSessionAuth");
+const { invokeCloudRunService } = require("../../config/authUtils");
 
-router.get("/", (req, res) => {
-  const qs = new URLSearchParams(req.query).toString();
-  res.cookie("my_app_params", qs, { httpOnly: true });
-  res.render("index", { qs });
+router.post("/", checkZendeskAuth, async (req, res, next) => {
+    try {
+        const qs = new URLSearchParams(req.query).toString();
+        const response = await invokeCloudRunService("/", "POST", req.body);
+        res.cookie("my_app_params", qs, { httpOnly: true });
+        res.render("index", { qs, data: response });
+    } catch (error) {
+        next(error); // Pass error to the error handler
+    }
+});
+
+router.get("/", checkSessionAuth, async (req, res, next) => {
+  try {
+      const qs = new URLSearchParams(req.query).toString();
+      const response = await invokeCloudRunService("/");
+      res.cookie("my_app_params", qs, { httpOnly: true });
+      res.render("index", { qs, data: response });
+  } catch (error) {
+      next(error); // Pass error to the error handler
+  }
 });
 
 module.exports = router;
