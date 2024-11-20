e73a4630474d84c6b3ff334d6460b9daad2fb21a
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-11-20 13:11:22 +0000

    Add check Session auth middleware and invoke Cloud Run Service

--- DC-Manager-App/app_remote/routes/ticketRoutes.js
+++ DC-Manager-App/app_remote/routes/ticketRoutes.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 const ticketController = require("../controllers/ticketController");
 const uploadAttachments = require("../middleware/uploadAttachments");
-const checkZendeskAuth = require("../middleware/zendeskAuth");
+const checkSessionAuth = require("../middleware/checkSessionAuth");
+const { invokeCloudRunService } = require("../../config/authUtils");
+
+router.use(checkSessionAuth);
 
 router.get("/new", ticketController.createTicket);
-router.post("/new", uploadAttachments, checkZendeskAuth, ticketController.submitTicket);
-router.get("/partials/:dataCenter", (req, res) => {
-  const dataCenter = req.params.dataCenter;
-  res.render(`partials/${dataCenter}-form`);
+router.post("/new", uploadAttachments, ticketController.submitTicket);
+router.get("/partials/:dataCenter", async (req, res, next) => {
+  try {
+    const response = await invokeCloudRunService("/partials/:dataCenter");
+    const dataCenter = req.params.dataCenter;
+    res.render(`partials/${dataCenter}-form`, { data: response });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
