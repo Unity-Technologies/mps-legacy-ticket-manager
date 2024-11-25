3aae8dcb87e024acecfe6e27c46e101b8a9698ea
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-11-25 14:00:05 +0000

    Remove Unauthenticated Endpoint

--- DC-Manager-App/app_remote/routes/dashboardRoutes.js
+++ DC-Manager-App/app_remote/routes/dashboardRoutes.js
@@ -4,16 +4,6 @@ const checkZendeskAuth = require("../middleware/zendeskAuth");
 const checkSessionAuth = require("../middleware/checkSessionAuth");
 const { invokeCloudRunService } = require("../../config/authUtils");
 
-router.post("/zendesk", async (req, res) => {
-    try {
-        const response = await invokeCloudRunService("/", "POST", req.body);
-
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: "Failed to forward request" });
-    }
-});
-
 router.post("/", checkZendeskAuth, async (req, res, next) => {
     try {
         const qs = new URLSearchParams(req.query).toString();
