faa6807e0ca7f17254d63b82d01530a020d330cf
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-12-02 16:45:24 +0000

    Remove invokeCloudRunService function from dynamic route

--- DC-Manager-App/app_remote/routes/ticketRoutes.js
+++ DC-Manager-App/app_remote/routes/ticketRoutes.js
@@ -3,20 +3,14 @@ const router = express.Router();
 const ticketController = require("../controllers/ticketController");
 const uploadAttachments = require("../middleware/uploadAttachments");
 const checkSessionAuth = require("../middleware/checkSessionAuth");
-const { invokeCloudRunService } = require("../../config/authUtils");
 
 router.use(checkSessionAuth);
 
 router.get("/new", ticketController.createTicket);
 router.post("/new", uploadAttachments, ticketController.submitTicket);
-router.get("/partials/:dataCenter", async (req, res, next) => {
-  try {
-    const response = await invokeCloudRunService("/partials/:dataCenter");
-    const dataCenter = req.params.dataCenter;
-    res.render(`partials/${dataCenter}-form`, { data: response });
-  } catch (error) {
-    next(error);
-  }
+router.get("/partials/:dataCenter", (req, res) => {
+  const dataCenter = req.params.dataCenter;
+  res.render(`partials/${dataCenter}-form`);
 });
 
 module.exports = router;
