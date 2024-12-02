fa52e12afece7ede8794a2c95442038f4651e550
Julian Titus-Glover <julian.titusglover@unity3d.com>
2024-12-02 16:44:02 +0000

    Remove invokeCloudRunService function

--- DC-Manager-App/app_remote/routes/dashboardRoutes.js
+++ DC-Manager-App/app_remote/routes/dashboardRoutes.js
@@ -2,14 +2,12 @@ const express = require("express");
 const router = express.Router();
 const checkZendeskAuth = require("../middleware/zendeskAuth");
 const checkSessionAuth = require("../middleware/checkSessionAuth");
-const { invokeCloudRunService } = require("../../config/authUtils");
 
 router.post("/", checkZendeskAuth, async (req, res, next) => {
     try {
         const qs = new URLSearchParams(req.query).toString();
-        const response = await invokeCloudRunService("/", "POST", req.body);
         res.cookie("my_app_params", qs, { httpOnly: true });
-        res.render("index", { qs, data: response });
+        res.render("index", { qs });
     } catch (error) {
         next(error); // Pass error to the error handler
     }
@@ -18,9 +16,8 @@ router.post("/", checkZendeskAuth, async (req, res, next) => {
 router.get("/", checkSessionAuth, async (req, res, next) => {
   try {
       const qs = new URLSearchParams(req.query).toString();
-      const response = await invokeCloudRunService("/");
       res.cookie("my_app_params", qs, { httpOnly: true });
-      res.render("index", { qs, data: response });
+      res.render("index", { qs });
   } catch (error) {
       next(error); // Pass error to the error handler
   }
